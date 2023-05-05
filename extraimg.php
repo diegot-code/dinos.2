cb76ac01d435cc5f1145c4bf8a646609c31b32c1
diego <126494563+torresd517@users.noreply.github.com>
2023-05-05 10:31:54 -0400

    Add files via upload

---
+++ extraimg.php
@@ -0,0 +1,145 @@
+<!-- <img
+  src="img/Dinos_in_Space_Draggables_Black_Hole.png"
+  id="draggable"
+  class="ui-widget-content"
+/> -->
+<!-- <img
+  src="img/Dinos_in_Space_Draggables_Alien_Planet.png"
+  id="draggable"
+  class="ui-widget-content"
+/> -->
+<!-- <img
+  src="img/Dinos_in_Space_Draggables_asteroid_floating.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_asteroid_shooting.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_crater.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_Earth.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_galaxy.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_jet_pack.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_Jupiter.png"
+  id="draggable"
+  class="ui-widget-content"
+/> -->
+<!-- <img
+  src="img/Dinos_in_Space_Draggables_Mars.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_Mars.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_Mercury.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_moon.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_Neptune.png"
+  id="draggable"
+  class="ui-widget-content"
+/> -->
+<!-- <img
+  src="img/Dinos_in_Space_Draggables_Rocket.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_rocket2.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_rocket_large.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_satellite.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_satellite2.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_Saturn.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_Shuttle1.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_shuttle2.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_space_person.png"
+  id="draggable"
+  class="ui-widget-content"
+/> -->
+<!-- <img
+  src="img/Dinos_in_Space_Draggables_star.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_sun.png"
+  id="draggable"
+  class="ui-widget-content"
+/> -->
+<!-- <img
+  src="img/Dinos_in_Space_Draggables_UFO.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_UFO_large.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_Uranus.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_Venus.png"
+  id="draggable"
+  class="ui-widget-content"
+/> -->
---
+++ img.php
@@ -0,0 +1,41 @@
+
+<img
+  src="img/Dinos_in_Space_Draggables_Brontosaurus.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+
+<img
+  src="img/Dinos_in_Space_Draggables_Kentrosaurus.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+
+<img
+  src="img/Dinos_in_Space_Draggables_Parasurolophus.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_Plesiosaurus.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+
+<img
+  src="img/Dinos_in_Space_Draggables_Spinosaurus.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+
+<img
+  src="img/Dinos_in_Space_Draggables_T-Rex.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+<img
+  src="img/Dinos_in_Space_Draggables_Triceratops.png"
+  id="draggable"
+  class="ui-widget-content"
+/>
+
